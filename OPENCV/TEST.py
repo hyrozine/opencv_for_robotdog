b8f9fceae6f167b9231acc439424e2d326c41650
hyrozine <henry74180260466@163.com>
2024-08-09 17:28:39 +0800

    fix

--- OPENCV/TEST.py
+++ OPENCV/TEST.py
@@ -5,12 +5,13 @@ import color_detect
 import ball_detect
 from line_detect import line_track
 
-video = cv2.VideoCapture(0)
+video = cv2.VideoCapture(0, cv2.CAO_V4L2)
 video.set(cv2.CAP_PROP_FRAME_WIDTH, img_size[0])
 video.set(cv2.CAP_PROP_FRAME_HEIGHT, img_size[1])
 
 while video.isOpened():
         ret,frame = video.read()
+        video.set(6, cv2.VideoWriter.fourcc('M', 'J', 'P', 'G'))
         if frame is None:
             break
         if ret == True:
