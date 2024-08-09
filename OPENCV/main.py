c241ca6ac2d117d64967a62111c0c0bde4d7485f
hyrozine <henry74180260466@163.com>
2024-08-09 17:03:17 +0800

    main

--- OPENCV/main.py
+++ OPENCV/main.py
@@ -1,21 +1,22 @@
 import cv2
+from utils import COLOR_THRESHOLD, img_size
 from state import state_machine
-img_size = (640, 480)
 
 if __name__ == '__main__':
+     
     video = cv2.VideoCapture(0)
     video.set(cv2.CAP_PROP_FRAME_WIDTH, img_size[0])
     video.set(cv2.CAP_PROP_FRAME_HEIGHT, img_size[1])
-    video.set(cv2.CAP_PROP_FPS, 30)
-    while video.isOpened():
-        ret,frame = video.read()
-        if frame is None:
-            break
-        if ret == True:
-            state_machine.state_machine_exe(frame)
 
-            key = cv2.waitKey(1)
-            if(key & 0xFF == ord('q')) :
+    while video.isOpened():
+            ret,frame = video.read()
+            if frame is None:
                 break
+            if ret == True:
+                state_machine.state_machine_exe(frame)
+                cv2.imshow('video',frame)
+                key = cv2.waitKey(1)
+                if(key & 0xFF == ord('q')) :
+                    break
 video.release()
 cv2.destroyAllWindows()
